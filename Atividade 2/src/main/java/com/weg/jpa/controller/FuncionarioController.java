5f590c747e5091313273f150179c4fa4bfe47458
gustaaxz <gustavo_hatschbach@estudante.sesisenai.org.br>
2026-06-23 13:46:44 -0300

    feat(Atividade 2): finalize controllers implementation

--- Atividade 2/src/main/java/com/weg/jpa/controller/FuncionarioController.java
+++ Atividade 2/src/main/java/com/weg/jpa/controller/FuncionarioController.java
@@ -1,5 +1,13 @@
 package com.weg.jpa.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("endereco")
+@RequiredArgsConstructor
 public class FuncionarioController {
     
 }
